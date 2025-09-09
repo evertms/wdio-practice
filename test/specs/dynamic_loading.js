908f3b0075278cfc91a36276ab687247b41744f0
Evert Moreno Serrate <evert23122004@gmail.com>
2025-09-09 09:01:25 -0400

    Update dynamic_loading.js
    Borrar describe y poner otro caso de prueba como "it", como lo indicó el docente

--- test/specs/dynamic_loading.js
+++ test/specs/dynamic_loading.js
@@ -32,23 +32,3 @@ describe('Prueba de carga dinámica', () => {
         await expect(helloWorldElement).toHaveText('Hello World!');
     });
 });
-
-/*describe('Prueba #2 de carga dinámica', () => {
-    it('Debe mostrar "Hello world!" siendo un elemento que se renderiza después de tocar "Start"', async () => {
-        await browser.url('https://the-internet.herokuapp.com/dynamic_loading');
-        const link2 = await $('#content a[href="/dynamic_loading/2"]');
-        await link2.click();
-
-        const startButton = await $('#start button');
-
-        await startButton.click();
-
-        const helloWorldElement = await $('#finish h4');
-        
-        await helloWorldElement.waitForDisplayed();
-
-        await expect(helloWorldElement).toBeDisplayed();
-        await expect(helloWorldElement).toHaveText('Hello World!');
-    });
-});
-*/
